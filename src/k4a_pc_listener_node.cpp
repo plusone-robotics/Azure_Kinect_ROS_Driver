086814ef871b2ae8931b8a11e555d6b24777eb1c
Shannon Stoehr <shannon.stoehr@plusonerobotics.com>
2023-05-15 10:54:52 -0600

    added messages to node, updated subscribe path

--- src/k4a_pc_listener_node.cpp
+++ src/k4a_pc_listener_node.cpp
@@ -4,16 +4,20 @@
 void p2Callback(const sensor_msgs::PointCloud2& msg)
 {
   // ROS_INFO("points2_listener heard: [%s]", msg->data.c_str());
-  ROS_INFO("we hearin things");
+  ROS_ERROR("we hearin things");
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "points2_listener");
-
+  
+  ROS_ERROR("testing");
+  
   ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe("points2", 100, p2Callback);
-
+  ros::Subscriber sub = nh.subscribe("/points2", 100, p2Callback);
+  
+  ROS_ERROR("should have subscribed by now");
+  
   ros::spin();
   return 0;
 }
