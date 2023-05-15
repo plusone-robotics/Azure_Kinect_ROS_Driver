4c44cb186c5b5b6719d489765b3d7394eacfe94c
Shannon Stoehr <shannon.stoehr@plusonerobotics.com>
2023-05-15 10:37:11 -0600

    sensor_msgs not string

--- src/k4a_pc_listener_node.cpp
+++ src/k4a_pc_listener_node.cpp
@@ -1,9 +1,10 @@
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "sensor_msgs/PointCloud2.h"
 
-void p2Callback(const std_msgs::String::ConstPtr& msg)
+void p2Callback(const sensor_msgs::PointCloud2& msg)
 {
-  ROS_INFO("points2_listener heard: [%s]", msg->data.c_str());
+  // ROS_INFO("points2_listener heard: [%s]", msg->data.c_str());
+  ROS_INFO("we hearin things");
 }
 
 int main(int argc, char **argv)
@@ -11,7 +12,7 @@ int main(int argc, char **argv)
   ros::init(argc, argv, "points2_listener");
 
   ros::NodeHandle nh;
-  ros::Subscriber sub = nh.subscribe<PointCloud2>("points2", 100, p2Callback);
+  ros::Subscriber sub = nh.subscribe("points2", 100, p2Callback);
 
   ros::spin();
   return 0;
