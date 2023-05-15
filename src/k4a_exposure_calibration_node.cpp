04b4b98f04d79a9ab9f86fa3e3fd8c0dfdecceb2
Shannon Stoehr <shannon.stoehr@plusonerobotics.com>
2023-05-15 16:24:52 -0600

    updated calibration node, starting dynamic reconfigure

--- src/k4a_exposure_calibration_node.cpp
+++ src/k4a_exposure_calibration_node.cpp
@@ -1,31 +1,44 @@
 #include "ros/ros.h"
 #include "sensor_msgs/PointCloud2.h"
 #include "sensor_msgs/Image.h"
+#include "dynamic_reconfigure/server.h"
+#include "Azure_Kinect_ROS_Driver/AzureKinectParams.h"
 
 void p2Callback(const sensor_msgs::PointCloud2& msg)
 {
-  // ROS_ERROR("calibration_listener (/points2) heard: [%s]", msg->data.c_str());
-  ROS_ERROR("exposure_calibration hearing points2");
+  ROS_INFO("exposure_calibration hearing /points2");
 }
 
 void rgbRawCallback(const sensor_msgs::Image& msg)
 {
-  // ROS_ERROR("calibration_listener (/rgb/raw/image) heard: [%s]", msg->data.c_str());
-  ROS_ERROR("exposure_calibration hearing rgb/raw/image");
+  ROS_INFO("exposure_calibration hearing /rgb/raw/image");
+}
+
+void calibrateExposure(azure_kinect_ros_driver::AzureKinectParamsConfig &config, uint32_t level)
+{
+  ROS_ERROR("Reconfigure request: %d",
+            config.exposure_time);
 }
 
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "k4a_exposure_calibration");
   
-  ROS_ERROR("initialized exposure calibration");
+  ROS_INFO("Initialized Exposure Calibration");
   
   ros::NodeHandle nh;
   ros::Subscriber subPC = nh.subscribe("/points2", 100, p2Callback);
   ros::Subscriber subRGBRaw = nh.subscribe("/rgb/raw/image", 100, rgbRawCallback);
   
-  ROS_ERROR("should have subscribed by now");
+  ROS_INFO("Exposure Calibration subscribed to /points2 and /rgb/raw/image");
   
+  dynamic_reconfigure::Server<azure_kinect_ros_driver::AzureKinectParamsConfig> server;
+  dynamic_reconfigure::Server<azure_kinect_ros_driver::AzureKinectParamsConfig>::CallbackType f;
+
+  f = boost::bind(&callback, _1, _2);
+  server.setCallback(f);
+
+  ROS_INFO("Spinning Exposure Calibration Node");
   ros::spin();
   return 0;
 }
