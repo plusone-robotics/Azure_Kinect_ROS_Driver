10550ec54c6e8d0bb85a04e47217564255ca0878
Shannon Stoehr <shannon.stoehr@plusonerobotics.com>
2023-05-17 15:56:14 -0600

    removed header

--- src/k4a_exposure_calibration_node.cpp
+++ src/k4a_exposure_calibration_node.cpp
@@ -3,7 +3,6 @@
 #include "sensor_msgs/Image.h"
 #include "dynamic_reconfigure/server.h"
 #include "azure_kinect_ros_driver/AzureKinectParamsConfig.h"
-#include "azure_kinect_ros_driver/k4a_exposure_calibration.h"
 
 void p2Callback(const sensor_msgs::PointCloud2& msg)
 {
