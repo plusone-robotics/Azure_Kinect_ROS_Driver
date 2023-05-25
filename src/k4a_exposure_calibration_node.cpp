7baa221d44a78dc8c806e939a9041bc6268ba3ab
Shannon Stoehr <shannon.stoehr@plusonerobotics.com>
2023-05-25 12:31:02 -0600

    added separate service to auto tune exposure

--- src/k4a_exposure_calibration_node.cpp
+++ src/k4a_exposure_calibration_node.cpp
@@ -16,11 +16,17 @@
 #include <k4a/k4a.h>
 #include <opencv2/opencv.hpp>
 
-bool firstk4aImageForExposureTuning = false;
+// make memory space to store latest image
+cv::Mat latest_k4a_image;
+cv::Mat* latest_k4a_image_ptr = &latest_k4a_image;
+// do we need to fetch latest image? true = yes
+// see sensor_manager.cpp lines 464/2018
+bool fetch_latest_k4a_image = true;
 
-bool k4aExposureTuning(int reqExposure)
+// call k4a_nodelet_manager/set_parameters to update exposure value
+bool k4aUpdateExposure(int reqExposure)
 {
-  ROS_INFO("Adjusting exposure_time to: [%d]", reqExposure);
+  ROS_INFO("Updating exposure_time to: [%d]", reqExposure);
 
   dynamic_reconfigure::ReconfigureRequest srv_req;
   dynamic_reconfigure::ReconfigureResponse srv_resp;
@@ -33,33 +39,38 @@ bool k4aExposureTuning(int reqExposure)
 
   srv_req.config = conf;
   ros::service::call("/k4a_nodelet_manager/set_parameters", srv_req, srv_resp);
-
+  // after updating the exposure we will need an updated latest image
+  fetch_latest_k4a_image = true;
   return true;
 }
 
-bool autoExposureTuning(cv::Mat &cvMat, int target_blue_value)
+// auto tune exposure with given target blue value
+bool k4aAutoTuneExposure(azure_kinect_ros_driver::AzureKinectParamsConfig &config,
+                         int target_blue_value)
 {
   ROS_ERROR("Starting auto exposure tuning...");
-  // split OpenCV mat into three color channels
-  cv::Mat color_channels[3];
-  cv::split(cvMat, color_channels);
-  // reminders: default exposure is 15625, min 488 max 1000000
-  int total_blue = 0;
-  int pixel_count = cvMat.rows * cvMat.cols;
   // exposure loop
   for(int exp=488; exp<1000000; exp+=500)
   {
-    bool autoTune = k4aExposureTuning(exp);
+    // split OpenCV mat into three color channels
+    cv::Mat color_channels[3];
+    cv::split(*latest_k4a_image_ptr, color_channels);
+    // reminders: default exposure is 15625, min 488 max 1000000
+    int total_blue = 0;
+    int rows = *latest_k4a_image_ptr.rows
+    int cols = *latest_k4a_image_ptr.cols
+    int pixel_count = rows * cols;
+    bool autoTune = k4aUpdateExposure(exp);
     if(!autoTune)
     {
-      ROS_ERROR("Unable to update exposure in autoExposureTuning");
+      ROS_ERROR("Unable to update exposure in k4aAutoTuneExposure");
       return false;
     }
     // calculate average blue value
     else{
-      for(int i=0; i<cvMat.rows; i++)
+      for(int i=0; i<rows; i++)
       {
-        for(int j=0; j<cvMat.cols; j++)
+        for(int j=0; j<cols; j++)
         {
           total_blue += color_channels[0].at<uchar>(i,j);
         }
@@ -70,42 +81,82 @@ bool autoExposureTuning(cv::Mat &cvMat, int target_blue_value)
     // did we achieve appropriate blue at this exposure
     if(current_avg_blue_value >= target_blue_value)
     {
-      ROS_ERROR("Successfully calibrated to blue value of [%d]", target_blue_value);
+      ROS_ERROR("Successfully calibrated exposure for blue value of [%d]", target_blue_value);
       break;
     }
   }
-
+  return true;
 }
 
-bool rosk4aExposureTuningCallback(azure_kinect_ros_driver::k4a_exposure_tuning::Request &req,
-                                  azure_kinect_ros_driver::k4a_exposure_tuning::Response &res)
+bool k4aUpdateExposureCallback(azure_kinect_ros_driver::AzureKinectParamsConfig &config,
+                               azure_kinect_ros_driver::k4a_exposure_tuning::Request &req,
+                               azure_kinect_ros_driver::k4a_exposure_tuning::Response &res)
 {
   // prepare response
   res.message = "";
 
   ROS_INFO("Received exposure tuning request: [%d]", req.new_exp);
-  ROS_INFO("Requesting exposure update to: [%d]", req.new_exp);
 
   // check exposure limits
   uint32_t req_exposure = req.new_exp;
-  uint32_t min_exposure = 488;
-  uint32_t max_exposure = 1000000;
+  uint32_t min_exposure = config.exposure_time.min;
+  uint32_t max_exposure = config.exposure_time.max;
 
   if(req_exposure < min_exposure || req_exposure > max_exposure)
   {
-    res.message += "Requested exposure out of range (488-1,000,000)";
+    res.message += ("Requested exposure out of range [%d] - [%d]", min_exposure, max_exposure);
     res.success = false;
     return true;
   }
   
-  bool tuningRes = k4aExposureTuning(req.new_exp);
+  bool tuningRes = k4aUpdateExposure(req.new_exp);
   
   ROS_INFO("Sending back response...");
 
   if(!tuningRes)
   {
     res.success = false;
-    res.message += "Unable to change exposure_time";
+    res.message += "Unable to update exposure_time, k4aUpdateExposure failed";
+    return true;
+  }
+  else
+  {
+    res.success = true;
+    res.updated_exp = req.new_exp;
+    res.message += "Exposure updated";
+    return true;
+  }
+}
+
+// insert lil wayne noises
+bool k4aAutoTuneExposureCallback(azure_kinect_ros_driver::k4a_exposure_tuning::Request &req,
+                                 azure_kinect_ros_driver::k4a_exposure_tuning::Response &res)
+{
+  // prepare response
+  res.message = "";
+
+  ROS_INFO("Received exposure auto tuning request: [%d]", req.target_blue_val);
+
+  // check exposure limits
+  uint32_t req_blue = req.target_blue_val;
+  uint32_t min_blue = 0;
+  uint32_t max_blue = 255;
+
+  if(req_blue < min_blue || req_blue > max_blue)
+  {
+    res.message += ("Requested blue out of range [%d] - [%d]", min_blue, max_blue);
+    res.success = false;
+    return true;
+  }
+  
+  bool autoTuningRes = k4aAutoTuneExposure(req.target_blue_val);
+  
+  ROS_INFO("Sending back response...");
+
+  if(!autoTuningRes)
+  {
+    res.success = false;
+    res.message += "Unable to auto tune exposure_time, k4aAutoTuneExposure failed";
     return true;
   }
   else
@@ -125,28 +176,25 @@ void p2Callback(const sensor_msgs::PointCloud2& msg)
 void rgbRawImageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
   ROS_DEBUG("exposure_calibration subscribed to /rgb/raw/image");
-  if(!firstk4aImageForExposureTuning)
+  if(fetch_latest_k4a_image)
   {
     try
     {
       // convert ROS image message to OpenCV
       cv_bridge::CvImageConstPtr CvImagePtr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::BGR8);
-      cv::Mat cvImage = CvImagePtr->image;
+      *latest_k4a_image_ptr = CvImagePtr->image;
 
       // check if conversion worked
-      if(cvImage.empty())
+      if(latest_k4a_image.empty())
       {
-        ROS_ERROR("Failed to convert k4a image to OpenCV mat");
+        ROS_ERROR("Failed to convert k4a rgb image to OpenCV mat");
       }
       else
       {
         ROS_ERROR("PRINTING IMAGE FOR FUNSIES");
-        cv::imshow("First Image", cvImage);
+        cv::imshow("Latest Image", latest_k4a_image);
         cv::waitKey(0);
-        firstk4aImageForExposureTuning = true;
-        int blue_value_requested = 255;
-        ROS_ERROR("Attempting autoTune with blue value of [%d]", blue_value_requested);
-        bool autoTuneAttempt = autoExposureTuning(cvImage, blue_value_requested);
+        fetch_latest_k4a_image = false;
       }
     }
     catch(cv_bridge::Exception& e)
@@ -169,8 +217,9 @@ int main(int argc, char **argv)
   image_transport::Subscriber subRGBRaw = it.subscribe("/rgb/raw/image", 1, rgbRawImageCallback);
   ROS_INFO("Exposure Calibration subscribed to /points2 and /rgb/raw/image");
 
-  // Advertise calibrate_exposure service
-  ros::ServiceServer service = nh.advertiseService("k4a_exposure_tuning", rosk4aExposureTuningCallback);
+  // Advertise services
+  ros::ServiceServer update_exposure_service = nh.advertiseService("k4a_update_exposure", k4aUpdateExposureCallback);
+  ros::ServiceServer auto_tune_exposure_service = nh.advertiseService("k4a_auto_tune_exposure", k4aAutoTuneExposureCallback);
   
   ros::Rate r(0.2);
   r.sleep();
