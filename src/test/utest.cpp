182a7245e3be0d10a00c114efc479b6ae36980d4
Shannon Stoehr <shannon.stoehr@plusonerobotics.com>
2023-06-02 13:47:04 -0600

    checking update exposure test

--- src/test/utest.cpp
+++ src/test/utest.cpp
@@ -9,72 +9,72 @@
 #include "azure_kinect_ros_driver/k4a_exposure_calibration_node.h"
 
 // fake image
-// cv::Mat k4aFakeImage;
+cv::Mat k4aFakeImage;
 
-// void publishk4aFakeImageData(ros::NodeHandle& pub_nh)
-// {
-//   // Create a fake RGB image (BGR8 format)
-//   cv::Mat image(480, 640, CV_8UC3, cv::Scalar(50, 0, 0));
-//   k4aFakeImage = image.clone();
+void publishk4aFakeImageData(ros::NodeHandle& pub_nh)
+{
+  // Create a fake RGB image (BGR8 format)
+  cv::Mat image(480, 640, CV_8UC3, cv::Scalar(50, 0, 0));
+  k4aFakeImage = image.clone();
 
-//   // Convert the image to ROS sensor_msgs/Image
-//   cv_bridge::CvImage cvImage;
-//   cvImage.header.stamp = ros::Time::now();
-//   cvImage.encoding = "bgr8";
-//   cvImage.image = image;
-//   sensor_msgs::ImagePtr msg = cvImage.toImageMsg();
+  // Convert the image to ROS sensor_msgs/Image
+  cv_bridge::CvImage cvImage;
+  cvImage.header.stamp = ros::Time::now();
+  cvImage.encoding = "bgr8";
+  cvImage.image = image;
+  sensor_msgs::ImagePtr msg = cvImage.toImageMsg();
 
-//   // Publish the fake image data
-//   image_transport::ImageTransport it(pub_nh);
-//   image_transport::Publisher pub = it.advertise("/rgb/raw/image", 1);
-//   pub.publish(msg);
-// }
+  // Publish the fake image data
+  image_transport::ImageTransport it(pub_nh);
+  image_transport::Publisher pub = it.advertise("/rgb/raw/image", 1);
+  pub.publish(msg);
+}
 
-// void publishk4aBadImageData(ros::NodeHandle& pub_nh)
-// {
-//   // mimic failure to convert k4a image to OpenCV mat
-//   cv::Mat image;
-//   k4aFakeImage = image.clone();
+void publishk4aBadImageData(ros::NodeHandle& pub_nh)
+{
+  // mimic failure to convert k4a image to OpenCV mat
+  cv::Mat image;
+  k4aFakeImage = image.clone();
 
-//   // Convert the empty image to ROS sensor_msgs/Image
-//   cv_bridge::CvImage cvImage;
-//   cvImage.header.stamp = ros::Time::now();
-//   cvImage.encoding = "bgr8";
-//   cvImage.image = image;
-//   sensor_msgs::ImagePtr msg = cvImage.toImageMsg();
+  // Convert the empty image to ROS sensor_msgs/Image
+  cv_bridge::CvImage cvImage;
+  cvImage.header.stamp = ros::Time::now();
+  cvImage.encoding = "bgr8";
+  cvImage.image = image;
+  sensor_msgs::ImagePtr msg = cvImage.toImageMsg();
 
-//   // Publish the fake image data
-//   image_transport::ImageTransport it(pub_nh);
-//   image_transport::Publisher pub = it.advertise("/rgb/raw/image", 1);
-//   pub.publish(msg);
-// }
+  // Publish the fake image data
+  image_transport::ImageTransport it(pub_nh);
+  image_transport::Publisher pub = it.advertise("/rgb/raw/image", 1);
+  pub.publish(msg);
+}
 
-// // Test case for k4aUpdateExposure in range
-// TEST(ExposureCalibrationTest, UpdateExposureInRangeTest)
-// {
-//   // Create a ROS node publisher handle
-//   ros::NodeHandle pub_nh;
+// Test case for k4aUpdateExposure in range
+TEST(ExposureCalibrationTest, UpdateExposureInRangeTest)
+{
+  // Create a ROS node publisher handle
+  ros::NodeHandle pub_nh;
 
-//   // Create a ROS node handle for testing class
-//   ros::NodeHandle test_nh;
-//   K4AExposureCalibration test_node(test_nh);
+  // Create a ROS node handle for testing class
+  ros::NodeHandle test_nh;
+  K4AExposureCalibration test_node(test_nh);
 
-//   // Publish fake image data
-//   publishk4aFakeImageData(pub_nh);
+  // Publish fake image data
+  publishk4aFakeImageData(pub_nh);
 
-//   // Wait for the image callback to update the latest_k4a_image
-//   ros::Duration(1.0).sleep();
+  // Wait for the image callback to update the latest_k4a_image
+  ros::Duration(1.0).sleep();
 
-//   // Call the k4aUpdateExposure function with a fake exposure value
-//   int error_code;
-//   std::string res_msg;
-//   bool result = test_node.k4aUpdateExposure(1000, error_code, res_msg);
+  // Call the k4aUpdateExposure function with a fake exposure value
+  int error_code;
+  std::string res_msg;
+  bool result = test_node.k4aUpdateExposure(1000, error_code, res_msg);
 
-//   // Check the result
-//   EXPECT_TRUE(result);
-//   EXPECT_EQ(error_code, azure_kinect_ros_driver::k4aCameraExposureServiceErrorCode::SUCCESS);
-//   k4aFakeImage.release();
-// }
+  // Check the result
+  EXPECT_TRUE(result);
+  EXPECT_EQ(error_code, azure_kinect_ros_driver::k4aCameraExposureServiceErrorCode::SUCCESS);
+  k4aFakeImage.release();
+}
 
 // // Test case for k4aUpdateExposure out of range (less than)
 // TEST(ExposureCalibrationTest, UpdateExposureOutOfRangeLTTest)
