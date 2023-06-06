68dff3c6f4db92675a567db7abc5e9efb27c23ea
Shannon Stoehr <shannon.stoehr@plusonerobotics.com>
2023-06-06 14:07:03 -0600

    fixing make file and test calls

--- src/test/utest.cpp
+++ src/test/utest.cpp
@@ -12,28 +12,33 @@ TEST(ExposureCalibrationTest, CameraExposureUpdateCheckTest)
 {
   ROS_ERROR("SANITY CHECK: CompareExposureTest");
   
-  K4AExposureCalibration test_node;
+  K4AExposureCalibration test_node_chExp;
   
   ROS_ERROR("SANITY CHECK: CompareExposureTest, test_node created");
 
-  int test_k4aExposureServiceErrorCode;
-  std::string test_message = "";
+  int test_k4aExposureServiceErrorCode_chExp;
+  std::string test_message_chExp = "";
 
   // test accurately changed appropriate exposure value 1000
   ROS_ERROR("SANITY CHECK: CompareExposureTest, about to call k4aCompare for okExp");
-  bool chExp = test_node.k4aCameraExposureUpdateCheck(1000, 1000, test_k4aExposureServiceErrorCode, test_message);
+  bool chExp = test_node_chExp.k4aCameraExposureUpdateCheck(1000, 1000, test_k4aExposureServiceErrorCode, test_message);
   ROS_ERROR("SANITY CHECK: CompareExposureTest, called k4aCompareExposure for okExp");
   ASSERT_TRUE(chExp);
-  ASSERT_TRUE(test_k4aExposureServiceErrorCode == azure_kinect_ros_driver::k4aCameraExposureServiceErrorCode::SUCCESS);
-  ASSERT_EQ(test_message, "Exposure update successful");
+  ASSERT_TRUE(test_k4aExposureServiceErrorCode_chExp == azure_kinect_ros_driver::k4aCameraExposureServiceErrorCode::SUCCESS);
+  ASSERT_EQ(test_message_chExp, "Exposure update successful");
+
+  K4AExposureCalibration test_node_unchExp;
+
+  int test_k4aExposureServiceErrorCode_unchExp;
+  std::string test_message_unchExp = "";
 
   // test unchanged appropriate exposure value (requested 1000, updated 15625 [default])
   ROS_ERROR("SANITY CHECK: CompareExposureTest, about to call k4aCompare for unchExp");
-  bool unchExp = test_node.k4aCameraExposureUpdateCheck(1000, 15625, test_k4aExposureServiceErrorCode, test_message);
+  bool unchExp = test_node_unchExp.k4aCameraExposureUpdateCheck(1000, 15625, test_k4aExposureServiceErrorCode, test_message);
   ROS_ERROR("SANITY CHECK: CompareExposureTest, called k4aCompareExposure for unchExp");
   ASSERT_FALSE(unchExp);
-  ASSERT_TRUE(test_k4aExposureServiceErrorCode == azure_kinect_ros_driver::k4aCameraExposureServiceErrorCode::CAMERA_EXPOSURE_SET_FAILURE);
-  ASSERT_EQ(test_message, "Failed to update exposure");
+  ASSERT_TRUE(test_k4aExposureServiceErrorCode_unchExp == azure_kinect_ros_driver::k4aCameraExposureServiceErrorCode::CAMERA_EXPOSURE_SET_FAILURE);
+  ASSERT_EQ(test_message_unchExp, "Failed to update exposure");
 }
 
 int main(int argc, char **argv)
