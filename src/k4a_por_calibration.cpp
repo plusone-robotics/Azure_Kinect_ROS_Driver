77b90f1577c37229c3758828c452f6eab89b2efd
Shannon Stoehr <shannon.stoehr@plusonerobotics.com>
2023-06-22 11:12:09 -0600

    subtract for updates

--- src/k4a_por_calibration.cpp
+++ src/k4a_por_calibration.cpp
@@ -325,12 +325,12 @@ bool K4APORCalibration::k4aSGDTune(const float target_blue_value,
       float white_error = k4aRMSE(white_avg, target_white_value);
 
       ROS_INFO("Current blue RMSE: [%f]", blue_error);
-      ROS_INFO("Current green RMSE: [%f]", green_avg);
-      ROS_INFO("Current red RMSE: [%f]", red_avg);
-      ROS_INFO("Current white RMSE: [%f]", white_avg);
+      ROS_INFO("Current green RMSE: [%f]", green_error);
+      ROS_INFO("Current red RMSE: [%f]", red_error);
+      ROS_INFO("Current white RMSE: [%f]", white_error);
 
       // update camera params
-      *exposure_time_double_ptr += LEARNING_RATE_ * white_error * dis(gen);
+      *exposure_time_double_ptr -= LEARNING_RATE_ * white_error * dis(gen);
       if(*exposure_time_double_ptr < MIN_EXPOSURE_)
       {
         exposure_time_uint = MIN_EXPOSURE_;
@@ -344,7 +344,7 @@ bool K4APORCalibration::k4aSGDTune(const float target_blue_value,
         exposure_time_uint = (uint32_t)*exposure_time_double_ptr;
       }
 
-      *white_balance_double_ptr += LEARNING_RATE_ * (blue_error + green_error + red_error) * dis(gen);
+      *white_balance_double_ptr -= LEARNING_RATE_ * (blue_error + green_error + red_error) * dis(gen);
       if(*white_balance_double_ptr < MIN_WHITE_BALANCE_)
       {
         white_balance_uint = MIN_WHITE_BALANCE_;
