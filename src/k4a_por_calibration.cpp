7d5783a7e219b66cadf4de990111f753636dd49c
Shannon Stoehr <shannon.stoehr@plusonerobotics.com>
2023-06-22 16:07:06 -0600

    we're gonna break it, learning rate 100

--- src/k4a_por_calibration.cpp
+++ src/k4a_por_calibration.cpp
@@ -353,7 +353,7 @@ bool K4APORCalibration::k4aSGDTune(const float target_blue_value,
       ROS_INFO("Current white RMSE: [%f]", white_error);
 
       // // update camera params
-      *exposure_time_double_ptr -= LEARNING_RATE_ * white_error * dis(gen);
+      *exposure_time_double_ptr += LEARNING_RATE_ * white_error * dis(gen);
       if(*exposure_time_double_ptr < MIN_EXPOSURE_)
       {
         exposure_time_uint = MIN_EXPOSURE_;
@@ -367,7 +367,7 @@ bool K4APORCalibration::k4aSGDTune(const float target_blue_value,
          exposure_time_uint = k4aStandardizeExposure((uint32_t)*exposure_time_double_ptr);
       }
 
-      *white_balance_double_ptr -= LEARNING_RATE_ * (blue_error + green_error + red_error) * dis(gen);
+      *white_balance_double_ptr += LEARNING_RATE_ * (blue_error + green_error + red_error) * dis(gen);
       if(*white_balance_double_ptr < MIN_WHITE_BALANCE_)
       {
         white_balance_uint = MIN_WHITE_BALANCE_;
