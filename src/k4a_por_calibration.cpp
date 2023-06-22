f5ab56c2246014dddc1983273d9de606f5f0bb96
Shannon Stoehr <shannon.stoehr@plusonerobotics.com>
2023-06-22 12:13:29 -0600

    added exposure calculations back in

--- src/k4a_por_calibration.cpp
+++ src/k4a_por_calibration.cpp
@@ -328,19 +328,19 @@ bool K4APORCalibration::k4aSGDTune(const float target_blue_value,
       ROS_INFO("Current white RMSE: [%f]", white_error);
 
       // // update camera params
-      // *exposure_time_double_ptr -= LEARNING_RATE_ * white_error;
-      // if(*exposure_time_double_ptr < MIN_EXPOSURE_)
-      // {
-      //   exposure_time_uint = MIN_EXPOSURE_;
-      // }
-      // else if(*exposure_time_double_ptr > MAX_EXPOSURE_)
-      // {
-      //   exposure_time_uint = MAX_EXPOSURE_;
-      // }
-      // else
-      // {
+      *exposure_time_double_ptr -= LEARNING_RATE_ * white_error;
+      if(*exposure_time_double_ptr < MIN_EXPOSURE_)
+      {
+        exposure_time_uint = MIN_EXPOSURE_;
+      }
+      else if(*exposure_time_double_ptr > MAX_EXPOSURE_)
+      {
+        exposure_time_uint = MAX_EXPOSURE_;
+      }
+      else
+      {
          exposure_time_uint = (uint32_t)*exposure_time_double_ptr;
-      // }
+      }
 
       *white_balance_double_ptr -= LEARNING_RATE_ * (blue_error + green_error + red_error) * dis(gen);
       if(*white_balance_double_ptr < MIN_WHITE_BALANCE_)
